056e9013a06d2c6d28b261b7d50d4ce5cb5625ca
moqasim <mqasim@bu.edu>
2021-09-03 10:28:37 -0400

    schema done

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -1,1 +1,13 @@
-your models/schemas go here
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  userName: String,
+  privLevel: Number,
+  lastLogin: { type: Date, default: Date.now },
+  email: String,
+  profilePic: String,
+});
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
