81418261d2f32ff6bca796c44b930578600a2a22
moqasim <mqasim@bu.edu>
2021-09-06 12:14:44 -0400

    updated userinfo

--- frontend-react/src/ components/UserInfo.jsx
+++ frontend-react/src/ components/UserInfo.jsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import usericon from "../usericon.png";
 
 function UserInfo(props) {
+  const [userDisplay, setUserDisplay] = useState("none");
+
+  function toggle() {
+    if (userDisplay == "block") {
+      setUserDisplay("none");
+    } else if (userDisplay == "none") {
+      setUserDisplay("block");
+    }
+  }
+
   return (
     <div>
       <div className="action">
         <div className="profile">
-          <img className="usericon" src={usericon} alt="usericon" />
+          <img
+            className="usericon"
+            src={usericon}
+            alt="usericon"
+            onClick={toggle}
+          />
         </div>
-        <div className="menu">
+        <div className="menu" style={{ display: userDisplay }}>
           <h3>
             {" "}
             Mohamed <br /> <span>Level 1 User</span>
