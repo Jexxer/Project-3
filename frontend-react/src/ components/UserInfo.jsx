72353ce0e024b05d3eb78a75873d64d526b72da9
moqasim <mqasim@bu.edu>
2021-09-06 09:24:48 -0400

    userinfo

--- frontend-react/src/ components/UserInfo.jsx
+++ frontend-react/src/ components/UserInfo.jsx
@@ -4,7 +4,28 @@ import usericon from "../usericon.png";
 function UserInfo(props) {
   return (
     <div>
-      <img className="usericon" src={usericon} alt="usericon" />
+      <div className="action">
+        <div className="profile">
+          <img className="usericon" src={usericon} alt="usericon" />
+        </div>
+        <div className="menu">
+          <h3>
+            {" "}
+            Mohamed <br /> <span>Level 1 User</span>
+          </h3>
+          <ul>
+            <li>
+              <a href="#">My Profile</a>
+            </li>
+            <li>
+              <a href="#">Account</a>
+            </li>
+            <li>
+              <a href="/">Logout</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
