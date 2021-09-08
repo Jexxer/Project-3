a96f23abc04abd3abb7d6230efc129c127a1764f
lmarin1197 <lmarin1197@gmail.com>
2021-09-08 19:48:21 -0400

    added styling/functionality to ticket

--- frontend-react/src/ components/Ticket.jsx
+++ frontend-react/src/ components/Ticket.jsx
@@ -4,47 +4,71 @@ import '../css/Ticket.css'
 export default function Ticket(props) {
     const [loading , setLoading] = useState(true)
     const [tickets , setTickets] = useState(null)
+    const [userInfo , setUserInfo] = useState(null)
 
     let url = `https://bugtracker-api-v1.herokuapp.com/api/tickets/${props.match.params.id}`
-
-    useEffect(() => {
+    let userUrl;
+    const ticketFetch = () =>{
         fetch(url)
         .then(res => res.json())
         .then(res => { 
             setTickets(res) 
-            setLoading(false)
+            //setLoading(false)
         })
-        .catch(err => { console.error(err) });        
-        
-    })
-    
+        .catch(err => { console.error(err) });
+    }
+    const userFetch = () =>{
+        if(tickets != null){
+            userUrl = `https://bugtracker-api-v1.herokuapp.com/api/users/${tickets.creatorId}`
+            fetch(userUrl)
+            .then(res => res.json())
+            .then(res => { 
+                setUserInfo(res)
+                console.log(userInfo) 
+                setLoading(false)
+            })
+            .catch(err => { console.error(err) });
+        }
+    }
+    useEffect(() => {
+        ticketFetch()
+    },[])
+
+    useEffect(() => {
+        userFetch()
+    },[tickets])
+
     if(loading){
         return null;
     }
-    else{
-        
+    else if(!loading && tickets != null){
+        console.log()
         return (
-            <div className = 'open-tickets'>
+            <div className = 'ticket-container'>
                 
                 <div className = 'ticket-header'>
-                    <div className = 'ticket-header-openclose'>Open Ticket</div>
-                </div>    
+                    <span className = 'ticket-header-openclose'>Open Ticket</span>
+                    <span className = 'update-ticket-status'>Update Ticket</span>
+                    <span className = 'close-ticket-button'>Close Ticket</span>
+                    <span></span>
+                </div>
                 <ul>
-                    <li id = 'title'>{tickets.title}</li>
-                    <li id = 'status'>{tickets.status}</li>
-                    <li id = 'id'>{tickets.creatorId}</li>
-                    <li id = 'message'>{tickets.message}</li>
-                    <li id = 'date'>{tickets.dateCreated}</li>
+                    <li><span>Email: </span>{userInfo.email}</li>
+                    <li id = 'title'><span>Title:</span> {tickets.title}</li>
+                    <li id = 'status'><span>Status:</span>{tickets.status}</li>
+                    <li id = 'id'><span>Creator's ID:</span>{tickets.creatorId}</li>
+                    <li id = 'date'><span>Created:</span>{tickets.dateCreated}</li>
+                    <li id = 'message'><span>Message:</span>{tickets.message}</li>
                 </ul>
                     {/* <button className = 'submit-bug'><Link to = '/tickets/new'>Submit Bug</Link></button> */}
                 
-                    
-                <div className = 'ticket-container'>
-                    
-                </div>
-                
             </div>
         );
+        
     }
 
 }
+//update status button
+//close ticket
+//section for dev notes(form)
+//submit button that pushes to a key in the ticket db (devnotes)
