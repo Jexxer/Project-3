216f932e16cb7385c989d0ddb20b346c5ad21657
jexxer <jexxe@live.com>
2021-09-08 09:26:43 -0400

    added hamburger menu

--- frontend-react/src/ components/NavBar.jsx
+++ frontend-react/src/ components/NavBar.jsx
@@ -1,9 +1,28 @@
 import React from "react";
 import "../css/NavBar.css";
+import {slide as Menu} from 'react-burger-menu'
 
 export default function NavBar(props) {
+
+  class Example extends React.Component {
+    showSettings (event) {
+      event.preventDefault();
+      
+      
+      
+    }
+  }
+
   return (
     <div className="navbar-container">
+
+      <Menu>
+        <a id="home" className="menu-item" href="/">Home</a>
+        <a id="about" className="menu-item" href="/about">About</a>
+        <a id="contact" className="menu-item" href="/contact">Contact</a>
+        <a  className="menu-item--small" href="">Settings</a>
+      </Menu>
+
       <div className="dashborad-btn">
 
           <a href="/">Dashboard</a>
